187bd97108378f3fb5ec69686df8c7ea5bd118f3
rob676 <malamaga63@gmail.com>
2018-11-21 19:44:39 +0200

    workshop2 added to repository

--- km-82/Hryhorenko_Kateryna/workshop1/source/main.py
+++ km-82/Hryhorenko_Kateryna/workshop1/source/main.py
@@ -1,8 +1,8 @@
-"""
-Тут написати умову до завдання
-"""
+import persha,zad.zukl1,laba1,laba13,zad.zukl2
+def zachi():
+    print(persha.control())#лабараторна 1 задача1
+    print(zad.zukl1.osnova())#лабараторна 2 задача 1
+    print(laba1.zad())#лабараторна 1 задача 2
+    print(laba13.raxynok())#лабараторна 1 задача 3
+    print(zad.zukl2.perevirka())#лабараторна 2 задча 2
 
-
-
-if __name__ == "__main__":
-    #TODO
